8e144037387870d35161bbc491116af18cc625fb
hilo <7amzalaaroussi@gmail.com>
2023-12-13 13:22:11 +0100

    add deleteUser service

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,15 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { UsersListType } from '../types/users.type';
+import { UserType, UsersListType } from '../types/users.type';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
-
+  perPage=6;
   constructor(private httpClient:HttpClient) { }
   getUsers=(pageNumber:number):Observable <UsersListType>=>{
-    return this.httpClient.get<UsersListType>(`https://reqres.in/api/users?page=${pageNumber}&per_page=6`);
+    return this.httpClient.get<UsersListType>(`${environment.apiLink}/users?page=${pageNumber}&per_page=${this.perPage}`);
+  }
+
+  deleteUser=(user:UserType):Observable<UsersListType>=>{
+    return this.httpClient.delete<UsersListType>(`${environment.apiLink}/users/${user.id}`); //prolly this is how it deletes it
   }
 }
