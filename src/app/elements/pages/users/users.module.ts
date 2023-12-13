8163d84266a76bc4873f3bfd76648c3902187f17
hilo <7amzalaaroussi@gmail.com>
2023-12-13 13:20:50 +0100

    add routing to add component and material modules for list component

--- src/app/elements/pages/users/users.module.ts
+++ src/app/elements/pages/users/users.module.ts
@@ -3,15 +3,30 @@ import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { ListComponent } from './list/list.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { AddComponent } from './add/add.component';
 
 
 @NgModule({
   declarations: [
-    ListComponent
+    ListComponent,
+    AddComponent
   ],
   imports: [
     CommonModule,
-    UsersRoutingModule
+    UsersRoutingModule,
+    TranslateModule,
+    NgSelectModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatIconModule
+    
   ]
 })
 export class UsersModule { }
