ca1f55d610a2edcc20affaa0dc6376c333052dbb
hilo <7amzalaaroussi@gmail.com>
2023-12-13 13:18:41 +0100

    added select for language and language selection logic

--- src/app/elements/header/header.component.ts
+++ src/app/elements/header/header.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit,OnDestroy{
+  @Output() selectedLanguage =new EventEmitter<string>()
+  languageSelected!:string;
+  languages:{}[]=[
+    {
+      flag:'../../../assets/icons/flags/icons8-great-britain-48.png',
+      name:'en'
+    },
+    {
+      flag:'../../../assets/icons/flags/icons8-france-48.png',
+      name:'fr'
+    }
+  ]
 
+  ngOnInit(): void {
+    this.languageSelected='en'
+    // console.log(this.languageSelected)
+  }
+
+  onSelect=(event:any)=>{
+    // console.log(this.languageSelected)
+    this.selectedLanguage.emit(this.languageSelected);
+  }
+  
+
+  ngOnDestroy(): void {
+    this.languageSelected=''
+  }
 }
